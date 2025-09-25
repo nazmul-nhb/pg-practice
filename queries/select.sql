2afe1f3f20b00bf438801d3cf23e7a3a2fbbd334
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-09-25 11:16:01 +0600

    🛠️ chore(select): explored query operators

---
+++ queries/select.sql
@@ -0,0 +1,15 @@
+SELECT * from hello_world WHERE hello_name = 'Hassan';
+
+select count(*) from students;
+
+SELECT * FROM students WHERE country IN ('USA', 'UK');
+
+SELECT * FROM students WHERE age BETWEEN 20 and 23 ORDER BY age ASC;
+
+SELECT * FROM students WHERE last_name LIKE '%son';
+
+SELECT * FROM students WHERE first_name ILIKE 'm_c%';
+
+-- LIMIT, OFFSET
+
+SELECT * FROM students LIMIT 8 OFFSET 0
